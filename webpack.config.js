b29c277ad687785d267b10b3b6ce28a0f7820a30
Ravi Tadinada <discursor@gmail.com>
2022-01-30 20:34:29 -0500

    Webpack initial commit

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+import * as path from 'path';
+import { fileURLToPath } from 'url'
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+
+
+const fileName = fileURLToPath(import.meta.url)
+const dirName = path.dirname(fileName)
+
+const config = {
+  mode: 'development',
+  devtool: 'inline-source-map',
+  devServer: {
+    static: './dist',
+  },
+  entry: {
+    index: './src/index.js',
+    print: './src/print.js',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Output Management',
+    }),
+  ],
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(dirName, 'dist'),
+    clean: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+};
+
+export default config;
